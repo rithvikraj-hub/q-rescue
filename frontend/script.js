09b0eeacaea93ed21ea4e87332e931a8299eafe7
rithvikraj-hub <rithvikraj2007@gmail.com>
2026-09-09 13:35:29 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function openDashboard() {
+    window.location.href = "dashboard.html";
+}
+
+function scrollToQuantum() {
+    document.getElementById("quantum").scrollIntoView({
+        behavior: "smooth"
+    });
+}
